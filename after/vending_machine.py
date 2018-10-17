26631f7feb3ba828c92169b5159adac186b7a4c6
RyujiTamaki <e44119rt@gmail.com>
2018-10-17 23:50:36 +0900

    リファクタリング用コード追加

---
+++ after/vending_machine.py
@@ -0,0 +1,75 @@
+from drink import Drink
+
+
+class VendingMachine:
+
+    def __init__(self):
+        self.quantity_of_coke = 5 # コーラの在庫数
+        self.quantity_of_diet_coke = 5 # ダイエットコーラの在庫数
+        self.quantity_of_tea = 5 # お茶の在庫数
+        self.number_of_100_yen = 10 # 100円玉の在庫
+        self.charge = 0 # お釣り
+
+    def buy(self, i, kind_of_drink):
+        '''
+        ジュースを購入する.
+        Parameters
+        ----------
+        i            : 投入金額. 100円と500円のみ受け付ける.
+        kind_of_drink: ジュースの種類. コーラ({@code Juice.COKE}),ダイエットコーラ({@code Juice.DIET_COKE},お茶({@code Juice.TEA})が指定できる.
+
+        Returns
+        -------
+        指定したジュース. 在庫不足や釣り銭不足で買えなかった場合は None が返される.
+        '''
+
+        # 100円と500円だけ受け付ける
+        if (i != 100) and (i != 500):
+            self.charge += i
+            return None
+
+        if (kind_of_drink == Drink.COKE) and (self.quantity_of_coke == 0):
+            self.charge += i
+            return None
+        elif (kind_of_drink == Drink.DIET_COKE) and \
+                (self.quantity_of_diet_coke == 0):
+            self.charge += i
+            return None
+        elif (kind_of_drink == Drink.TEA) and (self.quantity_of_tea == 0):
+            self.charge += i
+            return None
+
+        # 釣り銭不足
+        if i == 500 and self.number_of_100_yen < 4:
+            self.charge += i
+            return None
+
+        if i == 100:
+            # 100円玉を釣り銭に使える
+            self.number_of_100_yen += 1
+        elif i == 500:
+            # 400円のお釣り
+            self.charge += (i - 100)
+            # 100円玉を釣り銭に使える
+            self.number_of_100_yen -= (i - 100) / 100
+
+        if kind_of_drink == Drink.COKE:
+            self.quantity_of_coke -= 1
+        elif kind_of_drink == Drink.DIET_COKE:
+            self.quantity_of_diet_coke -= 1
+        else:
+            self.quantity_of_tea -= 1
+
+        return Drink(kind_of_drink)
+
+    def refund(self):
+        '''
+        お釣りを取り出す.
+
+        Returns
+        -------
+        お釣りの金額
+        '''
+        result = self.charge
+        self.charge = 0
+        return result
